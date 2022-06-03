988d5170ed9cd7ffa549f866d0e6234d0d06fa1f
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2022-06-03 08:36:27 -0400

    working at payment method

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  devise_for :admins
+  get "home/index"
+  root to: "home#index"
+  authenticate :admin do
+    resources :products
+    resources :categories
+  end
+
+  resource :cart, only: [:show, :update]
+end
